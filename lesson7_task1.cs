a1b34ba9eee4c0e6d5ef1d5f86bb5253794d9c0d
Alex Fresh <gemotransfuziya@mail.ru>
2024-11-06 22:19:32 +0500

    Переименовали файл program.cs в файл lesson7_task1

---
+++ lesson7_task1.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Используйте рекурсию и не используйте циклы.
+// Примеры
+// Вход: М = 1, N = 5
+// Выход: 1, 2, 3, 4, 5
+// Вход: М = 4, N = 8
+// Выход: 4, 5, 6, 7, 8
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = 4;
+        int n = 8;
+
+        PrintNaturalNumbersRecursively(m, n);
+        Console.WriteLine();
+    }
+
+    static void PrintNaturalNumbersRecursively(int currentNumber, int limit)
+    {
+        if (currentNumber > limit)
+        {
+            return;
+        }
+
+        Console.Write(currentNumber + " ");
+        PrintNaturalNumbersRecursively(currentNumber + 1, limit);
+    }
+}
+
